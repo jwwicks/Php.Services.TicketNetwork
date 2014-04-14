8b1bb01711aac3f2da13498c2812b0718532c870
John Wicks <jwwicks@gmail.com>
2014-04-13 21:30:25 -0700

    Additional methods to ticket class

--- ticketnetwork_ticket.class.php
+++ ticketnetwork_ticket.class.php
@@ -44,12 +44,17 @@ class TNTicket extends TicketNetworkBase{
 		$this->_request['method'] = 'GetTickets';
 	
 		if($params){
-			$this->set($params);
-			if(array_key_exists('eventID', $params)){
+
+			if(array_key_exists('method', $params)){
+				$this->_request['method'] = $params['method'];
+				unset($params['method']);
+			}else if(array_key_exists('eventID', $params)){
 				$this->_request['method'] = 'GetEventTickets';
 			}
+			
+			$this->set($params);
 		}
-	
+		
 		return $this;
 	}
 	
@@ -59,14 +64,31 @@ class TNTicket extends TicketNetworkBase{
 	 */
 	public function parse(){
 		$this->_data = array();
-	
+
 		try{
 			parent::parse();
-	
+						
 			if(!empty($this->_response)){
 				if($this->_request['method'] == 'GetTickets'){
 					if(is_array($this->_response->GetTicketsResult->TicketGroup)){
 						foreach($this->_response->GetTicketsResult->TicketGroup as $ticket){
+							$this->_data[] = $ticket;
+						}
+					}else{
+						$this->_data = null;
+					}
+				}else if($this->_request['method'] == 'GetTicketsCompressed'){
+					if(!empty($this->_response->GetTicketsCompressedResult)){
+						//$result = gzdecode($this->_response->GetTicketsCompressedResult);
+						foreach($this->_response->GetTicketsCompressedResult->Tickets->TicketGroup as $ticket){
+							$this->_data[] = $ticket;
+						}
+					}else{
+						$this->_data = null;
+					}
+				}else if($this->_request['method'] == 'GetEventTickets2'){
+					if(!empty($this->_response->GetEventTickets2Result->Tickets->TicketGroup2)){
+						foreach($this->_response->GetEventTickets2Result->Tickets->TicketGroup2 as $ticket){
 							$this->_data[] = $ticket;
 						}
 					}else{
@@ -81,9 +103,13 @@ class TNTicket extends TicketNetworkBase{
 						$this->_data = null;
 					}
 				}
+			}else{
+			    $this->_data = null;
 			}
 	
 		}catch(TNException $e){
+			$err = $this->debug($e_>getMessage());
+			echo $err;
 		}
 	
 		return $this;
